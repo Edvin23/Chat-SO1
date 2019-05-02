60d3e31f2a4530dca93622076d15037fd1d328dd
Edvin23 <39892584+Edvin23@users.noreply.github.com>
2019-05-01 23:46:41 -0600

    Update ClientList.java

--- ChatServer/src/chat/server/ClientList.java
+++ ChatServer/src/chat/server/ClientList.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package chat.server;
 
@@ -11,7 +7,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author danny
+ * @author saul
  */
 public class ClientList {
         static ArrayList<ClientHandler> listaUsuarios = new ArrayList<ClientHandler>();
