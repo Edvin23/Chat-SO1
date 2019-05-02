749ae9fe31ef997bb6a3014fbc07bdca94009f3b
Edvin23 <39892584+Edvin23@users.noreply.github.com>
2019-05-01 23:46:17 -0600

    Update ClientHandler.java

--- ChatServer/src/chat/server/ClientHandler.java
+++ ChatServer/src/chat/server/ClientHandler.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 
 package chat.server;
 
@@ -13,7 +9,7 @@ import java.net.Socket;
 
 /**
  *
- * @author danny
+ * @author saul
  */
 public class ClientHandler implements Runnable {
     private String username;
