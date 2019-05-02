b762f439b4cfa93667719dcf716115365ecef54f
Edvin23 <39892584+Edvin23@users.noreply.github.com>
2019-05-01 23:52:11 -0600

    Update Message.java

--- ChatServer/src/chat/Message.java
+++ ChatServer/src/chat/Message.java
@@ -1,11 +1,4 @@
-/*
 
-
-
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package chat;
 
@@ -13,7 +6,7 @@ import java.io.Serializable;
 
 /**
  *
- * @author danny
+ * @author saul
  */
 public class Message implements Serializable{
        private short code;
